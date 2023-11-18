7a0e72dfaa50d9be076c1a099075ab3691e3282e
demagoit <33demagoit@gmail.com>
2023-11-18 11:37:43 +0200

    setup.py created

---
+++ clean_folder/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='1',
+    description='Sorts folder contain to dediicated subfolders',
+    url='https://github.com/demagoit/M7_file_sorter_pack.git',
+    author='demagoit',
+    author_email='33demagoit@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    install_requires=['sys', 'shutil', 'pathlib'],
+    entry_points={'console_scripts': [
+        'clean_folder = clean_folder.clean:main']}
+)
